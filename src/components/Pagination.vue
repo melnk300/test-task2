9bee160be1c36550d96915a60c51678dcfee8e35
Руслан Мельник <melnk300@gmail.com>
2023-08-28 02:21:24 +0500

    [feature] Added pagination components

---
+++ src/components/Pagination.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {ArrowSmallLeftIcon, ArrowSmallRightIcon} from "@heroicons/vue/20/solid";
+
+type PaginationProps = {
+    pagesCount: number;
+    activePage: number;
+  }
+
+  defineProps<PaginationProps>()
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <button v-on:click="$emit('decrease-page')"><ArrowSmallLeftIcon class="h-6 w-6" /></button>
+    <button v-on:click="$emit('change-page', page)" v-for="page in pagesCount" class="rounded-full mx-3 h-6 w-6" :key="page" :class="activePage == page ? 'bg-amber-100' : 'bg-gray-100'">{{page}}</button>
+    <button v-on:click="$emit('increase-page')"><ArrowSmallRightIcon class="h-6 w-6" /></button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
