fffdeb9a40cec1b012ee78079ff997ee5b133610
Руслан Мельник <melnk300@gmail.com>
2023-08-28 02:22:07 +0500

    [feature] Added project component

---
+++ src/components/Project.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import type {project} from "@/dto/project";
+
+  defineProps<project>()
+
+  const colors = {
+    CREATED: 'bg-gray-100',
+    IN_PROGRESS: 'bg-green-100',
+    FINISHED: 'bg-blue-100'
+  }
+</script>
+
+<template>
+  <div class="content">
+    <div class="grid grid-cols-7">
+      <div class="col-span-1 border py-3 px-4">{{id}}</div>
+      <div class="col-span-4 border py-3 px-4">{{title}}</div>
+      <div class="col-span-2 border py-3 px-4">
+        <span class="p-2 rounded-xl" :class="colors[status]">{{status}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
